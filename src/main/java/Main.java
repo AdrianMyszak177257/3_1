7a6ffe2d45202771c6763489a68292c0cc188501
AdrianMyszak177257 <177257@stud.prz.edu.pl>
2025-04-09 12:30:03 +0000

    3_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,29 @@
 
 // import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj liczbę wierszy gwiazdek:");
+        int rows = scanner.nextInt();
+
+        for (int i = 1; i <= rows; i++) { 
+            for (int j = 1; j <= i; j++) { 
+                System.out.print("*");
+            }
+            System.out.println(); 
+        }
+
+        scanner.close();
+    }
+}
+
+
 
   // @Test
   // void addition() {
   //     assertEquals(2, 1 + 1);
   // }
-}
